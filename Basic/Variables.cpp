4e3b928c697b06dbc16e5920d52afee1d4ae7b85
Utkarsh Mishra <utkarshmishrapw100.com>
2026-08-14 12:14:34 +0530

    Question no.2 Variables

--- Basic/Variables.cpp
+++ Basic/Variables.cpp
@@ -1,3 +1,4 @@
+//  Question no. 1 -- Variables
 #include <iostream>
 #include <string>
 using namespace std;
@@ -16,4 +17,21 @@ int main() {
     cout << "Enter your grade (A-F): ";
     cin >> grade;
     cout << "Hello, " << name << "! You are " << age << " years old, " << height << " ft tall, and your grade is " << grade << "." << endl;
+
+
+// Question no. 2 -- Variables & Calculations
+
+    string Name;
+    getline(cin, Name);
+    int Age;
+    cin >> Age;
+    int Physics;
+    cin >> Physics;
+    int Chemistry;
+    cin >> Chemistry;
+    int Math;
+    cin >> Math;
+    int Total = Physics + Chemistry + Math;
+    float Average = Total / 3.0;
+    cout << "Hello, " << Name << "! You are " << Age << " years old, and your total marks are " << Total << " with an average of " << Average << "." << endl;
 }
